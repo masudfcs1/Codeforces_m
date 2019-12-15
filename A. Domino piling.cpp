a0def318d3319cd1abe9dceead072f7301c4e837
masudfcs1 <57311382+masudfcs1@users.noreply.github.com>
2019-12-15 17:47:12 +0600

    Create A. Domino piling.cpp

---
+++ A. Domino piling.cpp
@@ -0,0 +1,15 @@
+include<iostream>
+#include<math.h>
+using namespace std;
+bool comp(int a, int b)
+{
+return(a<b);
+}
+int main()
+{
+    long int ans,a,b;
+    cin>>a>>b;
+ 
+    cout<<(a*b)/2<<endl;
+    return 0;
+}
